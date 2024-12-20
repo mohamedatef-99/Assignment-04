e51471916cc9fb0bdd43560edb202b073d0027b2
Mohamed Atef <Mohamed Atef>
2024-12-20 20:54:19 +0200

    One D Array

--- Assignment 04/Program.cs
+++ Assignment 04/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region One D Array
+            // One D Array
+            //   int[] Number;
+            //   // Declare For Reference from type array of integers
+            //   // Numbers can refere to object of type array of integers
+            //   // numbers refer to null
+
+            //   // 8 bytes will be allocated at stack for reference numbers
+            //   // 0 bytes will be allocated at heap for array of integers
+
+            //   Number = new int[5];
+
+            //   // new
+            //   // 1. allocate the number of required bytes for the object at heap (4 byets * 5) = 20
+            //   // 2. Initialize the allocated block of memory to default values (0 for integers)
+            //   // 3. call user defined constructor if exsits
+            //   // 4. assign the object to the reference numbers
+
+            //   Console.WriteLine(Number[0]);
+
+            //   int[] num = { 1, 2, 3, 4, 5 };
+
+
+            //   int[] num01;
+            //   for (int i = 0; i < num.Length; i++)
+            //   {
+            //       Console.WriteLine(num[i]);
+            //   }
+
+            //   Console.WriteLine(num.Length);
+            //   Console.WriteLine(num.Rank); 
+            #endregion
+
         }
     }
 }
