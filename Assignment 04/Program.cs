82cbb91b7afdedfff4fd89ab80afcd9a2221b122
Mohamed Atef <Mohamed Atef>
2024-12-21 20:03:58 +0200

    Q1

--- Assignment 04/Program.cs
+++ Assignment 04/Program.cs
@@ -2,9 +2,51 @@
 {
     internal class Program
     {
+        #region Q1
+        //static void Swap(int x, int y)
+        //{
+        //    int temp = x;
+        //    x = y;
+        //    y = temp;
+        //}
+        //static void SwapRef(ref int x, ref int y)
+        //{
+        //    int temp = x;
+        //    x = y;
+        //    y = temp;
+        //} 
+        #endregion
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            ////1.Passing by value
+
+            //int A = 4;
+            //int B = 5;
+            //Console.WriteLine("Before Swap");
+            //Console.WriteLine(A);
+            //Console.WriteLine(B);
+
+            //Swap(A, B);
+            //Console.WriteLine("After Swap");
+            //Console.WriteLine(A);
+            //Console.WriteLine(B);
+
+
+            ////2.Passing by reference
+            //int x = 4;
+            //int Y = 5;
+            //Console.WriteLine("Before Swap Ref");
+            //Console.WriteLine(x);
+            //Console.WriteLine(Y);
+
+            //SwapRef(ref x, ref Y);
+            //Console.WriteLine("After Swap Ref");
+
+            //Console.WriteLine(x);
+            //Console.WriteLine(Y); 
+            #endregion
         }
     }
 }
